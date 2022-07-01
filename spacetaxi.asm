1269bb093d2c1bccf5111d9b52eaa41b2be185af
Maciej Malecki <maciej.malecki@capgemini.com>
2022-07-01 08:28:44 +0200

    title screen

--- spacetaxi.asm
+++ spacetaxi.asm
@@ -1,5 +1,6 @@
+#import "build/charpad/metadata.asm"
 
-.label SCREEN = $1000
+.label SCREEN = $0400
 .label COLOUR_RAM = $D800
 
 
@@ -9,11 +10,23 @@ BasicUpstart(start) // Basic start routine
 // Main program
 *=$080d "Program"
 
-start:
-    lda #BLACK
+start: {
+    jsr initScreen
+    jsr drawTitleScreen
+    rts
+}
+
+// game initialization
+initScreen: {
+    lda #backgroundColour0
     sta $D020
     sta $D021
+    lda #backgroundColour1
+    sta $D022
+    lda #backgroundColour2
+    sta $D023
     rts
+}
 
 
 // ==== screen drawing routines ====
@@ -25,37 +38,59 @@ start:
  * DESTROYS: A, X, Y
  */
 .macro copyScreenBlock(width, screenTarget, colorSource, colorTarget) {
-    sta sourceAddressStorage
-    stx sourceAddressStorage + 1
+    sta sourceAddress
+    stx sourceAddress + 1
 
     .for(var line = 0; line < 25; line++) {     // unroll outer loop
         ldx #width
-        lda sourceAddressStorage
-        sta sourceAddress
-        lda sourceAddressStorage + 1
-        sta sourceAddress + 1
         !:
             dex
-            lda sourceAddress:$ffff,x          // load source char code
-            tay                                 // keep char code in Y
+            jsr loadSource                  // load source char code
+            tay                             // keep char code in Y
             sta line*40 + screenTarget,x    // copy char code
-            lda colorSource,y                   // decode color for given char code (now in Y)
+            lda colorSource,y               // decode color for given char code (now in Y)
             sta line*40 + colorTarget,x     // set given color RAM location
             cpx #0
         bne !-
     }
     rts
-    sourceAddressStorage: .word $0000
+
+    loadSource:
+        lda sourceAddress:$ffff,x
+        cpx #0
+        bne !+
+            pha
+            clc
+            lda #width
+            adc sourceAddress
+            sta sourceAddress
+            lda #0
+            adc sourceAddress + 1
+            sta sourceAddress + 1
+            pla
+        !:
+    rts
 }
 
 copyFullScreen: copyScreenBlock(40, SCREEN, colours, COLOUR_RAM)
 copyDashboard: copyScreenBlock(11, SCREEN + 29, colours, COLOUR_RAM + 29)
 copyLevel: copyScreenBlock(29, SCREEN, colours, COLOUR_RAM)
 
+drawTitleScreen: {
+    lda #<titleScreen
+    ldx #>titleScreen
+    jsr copyFullScreen
+    rts
+}
+
 // ==== data ====
 
 colours:        .import binary "build/charpad/colours.bin"
+charset:        .import binary "build/charpad/charset.bin"
 titleScreen:    .import binary "build/charpad/title.bin"
 dashboard:      .import binary "build/charpad/dashboard.bin"
 level1:         .import binary "build/charpad/level1.bin"
 level2:         .import binary "build/charpad/level2.bin"
+
+end:
+.print "Program size = " + (end - start + 1)
